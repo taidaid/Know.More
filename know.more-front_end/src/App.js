aabfd2563f5a4982bea28a5fb2a837ae45646af4
Bryan Windsor <bryan@Bryans-MacBook-Air.local>
2018-12-06 21:00:17 +0100

    style, buttons, and cards

--- know.more-front_end/src/App.js
+++ know.more-front_end/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import Card from './components/Card/Card';
 
 class App extends Component {
   render() {
@@ -12,12 +14,16 @@ class App extends Component {
             <div className='Welcome'>
               <h2>Know more about your music</h2>
               <div className='Welcome-buttons'>
-                <button>Sign Up</button>
-                <button>Login</button>
+                <button className='waves-effect waves-light btn'>Sign Up</button>
+                <button className='waves-effect waves-light btn'>Login</button>
               </div>
             </div>
             <div className="App-footer">Powered by Spotify API</div>
           </div>
+        </div>
+        <div className='Welcome-info'>
+          <Card />
+          <Card />
         </div>
       </div>
     );
